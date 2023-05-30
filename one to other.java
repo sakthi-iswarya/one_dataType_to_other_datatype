4b395c6f2a4e217f15d9280883a5fb0ad3319245
Sakthi Iswarya S <82805914+sakthi-iswarya@users.noreply.github.com>
2023-05-30 15:40:13 +0530

    Add files via upload

---
+++ one to other.java
@@ -0,0 +1,13 @@
+//int to double 
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x =sc.nextInt();
+        double x1 =(double) x;
+        System.out.println(x1);
+        double e=sc.nextDouble();
+        int e2 =(int) e;
+        System.out.println(e2);
+    }
+}
